6d97c4a68c7530c68f9244bf8d0dbb668b34fcab
iamageo <geovani.af4@gmail.com>
2022-07-13 17:51:39 -0300

    example of use isCpf

--- app/src/main/java/com/iamageo/brazilian_toolkit/MainActivity.kt
+++ app/src/main/java/com/iamageo/brazilian_toolkit/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.iamageo.brazilian_toolkit
 
+import com.iamageo.brazilian_toolkit_library.isCpf
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import com.iamageo.brazilian_toolkit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupValidateCpf()
+    }
+
+    private fun setupValidateCpf() {
+        binding.btnVerify.setOnClickListener {
+            if(!binding.textFieldCpf.editText?.text.toString().isCpf()) {
+                binding.textFieldCpf.error = "Cpf inválido!"
+            } else {
+                Toast.makeText(this, "Cpf válido!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
