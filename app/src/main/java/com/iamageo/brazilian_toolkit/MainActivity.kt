67bc59e4773203d0591b82de49f55954e1c0cf02
iamageo <geovani.af4@gmail.com>
2022-07-14 11:16:52 -0300

    example of use CPF and CNPJ validation with mask

--- app/src/main/java/com/iamageo/brazilian_toolkit/MainActivity.kt
+++ app/src/main/java/com/iamageo/brazilian_toolkit/MainActivity.kt
@@ -5,8 +5,9 @@ import com.iamageo.brazilian_toolkit_library.isCpf
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.core.widget.doOnTextChanged
 import com.iamageo.brazilian_toolkit.databinding.ActivityMainBinding
+import com.iamageo.brazilian_toolkit_library.BrazilianUtilsMask
+import com.iamageo.brazilian_toolkit_library.isCnpj
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +22,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupValidateCpf() {
+
+        binding.textFieldCpf.editText?.addTextChangedListener(BrazilianUtilsMask.cpfAndCnpjMask(binding.textFieldCpf.editText!!))
+
         binding.btnVerify.setOnClickListener {
-            if(!binding.textFieldCpf.editText?.text.toString().isCpf()) {
-                binding.textFieldCpf.error = "Cpf inválido!"
-            } else {
-                Toast.makeText(this, "Cpf válido!", Toast.LENGTH_SHORT).show()
+            when(binding.textFieldCpf.editText?.text?.length!!) {
+                14 -> {
+                    if(!binding.textFieldCpf.editText?.text.toString().isCpf()) {
+                        binding.textFieldCpf.error = "Cpf inválido!"
+                    } else {
+                        binding.textFieldCpf.error = ""
+                        Toast.makeText(this, "Cpf válido!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                18 -> {
+                    if(!binding.textFieldCpf.editText?.text.toString().isCnpj()) {
+                        binding.textFieldCpf.error = "Cnpj inválido!"
+                    } else {
+                        binding.textFieldCpf.error = ""
+                        Toast.makeText(this, "Cnpj válido!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else -> {
+                    Toast.makeText(this, "Não é um CPF nem CNPJ válido", Toast.LENGTH_SHORT).show()
+                }
             }
+
         }
     }
 }
